29b858720c414441b12ecd8d3cf5903579f689c5
Ivan Pintado <ivan.pintadoh@gmail.com>
2017-08-25 19:09:23 -0700

    Replace phantomjs with puppeteeer

--- api.js
+++ api.js
@@ -1,12 +1,10 @@
 'use strict';
 /* eslint-env browser */
-const driver = require('promise-phantom');
-const phantomjs = require('phantomjs-prebuilt');
+const puppeteer = require('puppeteer');
 const Observable = require('zen-observable');
 
 function init(page, observer, prevSpeed) {
-	// TODO: Doesn't work with arrow function. open issue on `promise-phantom`
-	page.evaluate(function () { // eslint-disable-line prefer-arrow-callback
+	page.evaluate(() => {
 		const $ = document.querySelector.bind(document);
 
 		return {
@@ -32,9 +30,9 @@ function init(page, observer, prevSpeed) {
 }
 
 module.exports = () => new Observable(observer => {
-	driver.create({path: phantomjs.path})
-		.then(phantom => phantom.createPage())
-		.then(page => page.open('http://fast.com').then(() => {
+	puppeteer.launch()
+		.then(browser => browser.newPage())
+		.then(page => page.goto('https://fast.com').then(() => {
 			init(page, observer);
 		}))
 		.catch(err => observer.error(err));
